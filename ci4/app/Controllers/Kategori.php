a31633be5e461a1aef8844d0a4abb5a7dc469d1c
Alang Satrio <alangsatrio123@gmail.com>
2020-11-16 08:27:56 +0700

    1.0

---
+++ ci4/app/Controllers/Kategori.php
@@ -0,0 +1,36 @@
+<?php namespace App\Controllers;
+
+class Kategori extends BaseController
+{
+	public function index()
+	{
+        echo "<h1>Belajar CI4</h1>";
+    }
+    
+    public function select(){
+
+    }
+
+    public function selectWhere($id = null){
+
+    }
+
+    public function formInsert(){
+
+    }
+
+    public function formUpdate(){
+
+    }
+
+    public function update($id = null){
+
+    }
+
+    public function delete($id = null){
+        
+    }
+
+	//--------------------------------------------------------------------
+
+}
